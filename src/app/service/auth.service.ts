9f57e0f886ecee2f1a43a162b588124c4a5f81b6
leticia paoleschi <paoleschileticia480@gmail.com>
2022-05-05 17:32:26 -0300

    componente inicio sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -22,4 +24,15 @@ export class AuthService {
     return this.http.post<User>('https://blogpessoalthiago.herokuapp.com/usuarios/cadastrar', user)
 
   }
+
+  logado(){
+    let ok: boolean = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok
+  }
+
 }
