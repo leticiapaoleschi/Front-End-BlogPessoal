903ceec7f309f9f0a0352e463e85d42757bb557f
leticia paoleschi <paoleschileticia480@gmail.com>
2022-05-09 14:17:09 -0300

    implementação do get e post de tema

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -17,11 +17,11 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-  return this.http.post<UserLogin>('https://blogpessoalthiago.herokuapp.com/usuarios/logar', userLogin)
+  return this.http.post<UserLogin>('http://localhost:8080/usuario/logar', userLogin)
   }
 
   cadastrar( user: User): Observable<User>{
-    return this.http.post<User>('https://blogpessoalthiago.herokuapp.com/usuarios/cadastrar', user)
+    return this.http.post<User>('http://localhost:8080/usuario/cadastrar', user)
 
   }
 
